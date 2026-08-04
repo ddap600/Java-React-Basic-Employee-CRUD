787cf1d4651e59fd866f2f8ffcc787ab764c4782
Ddap <diogopinto728@gmail.com>
2026-08-04 20:45:08 +0000

    added react router and configured route too employee list

--- employee-frontend/src/App.jsx
+++ employee-frontend/src/App.jsx
@@ -1,20 +1,24 @@
-import './App.css'
-import './HelloWorld.jsx'
-import HelloWorld from "./HelloWorld.jsx";
+import "./App.css";
+import "./HelloWorld.jsx";
 import ListEmployee from "./components/ListEmployee.jsx";
 import Header from "./components/Header.jsx";
-import Footer from './components/Footer.jsx';
+import Footer from "./components/Footer.jsx";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-
   return (
     <>
-      <Header />
-      <HelloWorld />
-      <ListEmployee />
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<ListEmployee />} />
+          <Route path="/employee" element={<ListEmployee />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
